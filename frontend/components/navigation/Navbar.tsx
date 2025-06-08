2adb16be0db8357460e5b60f7c6a9e1786e2f61f
ProdFilthi <gamerfilthi@gmail.com>
2025-06-08 13:15:14 +0200

    Refactor Navbar component: adjust layout for logo and improve sign-in button integration

--- frontend/components/navigation/Navbar.tsx
+++ frontend/components/navigation/Navbar.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -22,7 +23,7 @@ export default function Navbar() {
     <header className="fixed top-0 w-full right-0 left-0 py-3 z-50 backdrop-blur-lg rounded-b-lg">
       <nav className="flex items-center justify-between px-20">
         <Link href={"/"}>
-          <div className="flex items-center -space-x-2 justify-center">
+          <div className="flex items-center -space-x-2 justify-between">
             <Image
               src={"/dirty-bucket-logo.svg"}
               alt="dirty bucket logo"
@@ -39,6 +40,19 @@ export default function Navbar() {
             </Link>
           ))}
         </ul>
+        <SignedOut>
+          <SignInButton>
+            <button
+              type="button"
+              className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition-all duration-200 cursor-pointer shadow-md"
+            >
+              Sign In
+            </button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </nav>
     </header>
   );
