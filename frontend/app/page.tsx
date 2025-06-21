10f0c9067a3f2c75c1ff76718f843bc27e6c38c6
ProdFilthi <gamerfilthi@gmail.com>
2025-06-21 11:26:33 +0200

    Update homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,21 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className="flex items-center justify-center font-black mt-24">
-      <h1 className="text-4xl">Home Page</h1>
+    <div className="flex items-center justify-center min-h-screen flex-col">
+      <h1 className="text-3xl md:text-4xl lg:text-6xl font-semibold mb-4">
+        Are you tired of overpriced junk?
+      </h1>
+      <div className="pb-8 opacity-75">
+        <p className="text-xl text-center">This is your marketplace</p>
+        <p className="text-2xl text-center">And a digital home to sell</p>
+        <p className="text-3xl">Your beats & keep your money!</p>
+      </div>
+      <button
+        type="button"
+        className="bg-green-500 text-white p-2 w-64 md:w-32 md:rounded-sm rounded-lg cursor-pointer"
+      >
+        Try it Out!
+      </button>
     </div>
   );
 }
